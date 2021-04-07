5393ab071c84070aa1bb8bbc5217d6e679552388
Matthew Faris <mtfaris90@gmail.com>
2021-04-07 17:51:43 -0400

    remove unused boilerplate and set up repo structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+import Login from "./Login.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Fluz Take Home Challenge</header>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import "../styles/Login.css"
+
+function Login() {
+  return (
+    <div className="Login">
+        Login Page
+    </div>
+  );
+}
+
+export default Login;
